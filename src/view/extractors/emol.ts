9472a3dfd94dc12aa709ca3cdf5ae8bb563f7dbc
VRAraya <victor.arayar@utem.cl>
2020-11-24 16:57:54 -0300

    arreglando bug en el back menu

--- src/view/extractors/emol.ts
+++ src/view/extractors/emol.ts
@@ -1,32 +1,16 @@
 import { backOrExit, extractorInfo, termmOrBackOrExit } from '@/helpers/input';
 import extractors from 'ea-core-gpi-pi';
-import { vUrl } from 'ea-common-gpi-pi'
-
-async function getURL() {
-	let url:string;
-	while (!url) {
-		const newUrl = await termmOrBackOrExit('Ingrese URL de la noticia');
-		if (newUrl === 0) throw new Error('Invalid URL');
-		const isValid = vUrl()(newUrl);
-		if (typeof isValid === 'boolean') {
-			url = newUrl;
-		} else {
-			console.error(isValid);
-		}
-	}
-	console.log('URL: ', url);
-	return url;
-}
 
 export default async (): Promise<void> => {
 	const emol = extractors.get('emol-extractor');
 	const back = true;
 	while (back) {
 		extractorInfo(emol);
+		const url = await termmOrBackOrExit('Ingrese la URL de la noticia');
+		if (url === 0) return;
 		try {
-			await emol.deploy();
 			console.clear();
-			const url = await getURL();
+			await emol.deploy();
 			const result = await emol.obtain({
 				limit: 1000,
 				metaKey: url
